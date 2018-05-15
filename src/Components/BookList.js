bea7c83d15c2a4a64de833c164289ee6ce826387
Laurent Milne <lmilne360@gmail.com>
2018-05-15 14:20:49 -0400

    added pagination library and updated book list

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,30 +1,68 @@
 import React, { Component } from "react";
 import Book from "./Book";
+import JwPagination from "jw-react-pagination";
 
+// import { Link } from "react-router";
 
+const customLabels = {
+  first: "<<",
+  last: ">>",
+  previous: "<",
+  next: ">"
+};
 export default class BookList extends Component {
-
   constructor(props) {
     super(props);
+    this.onChangePage = this.onChangePage.bind(this);
+    this.onBookSelect = this.onBookSelect.bind(this);
+    this.state = {
+      exampleItems: this.props.books,
+      pageOfItems: [],
+      bookSelected: false
+    };
+  }
+  onChangePage(pageOfItems) {
+    this.setState({ pageOfItems: pageOfItems });
+  }
 
+  onBookSelect(index) {
+    console.log("book selected");
+    if (this.state.bookSelected) {
+      return this.setState({ bookSelected: false, selectedBook: null });
+    }
+    const book = this.state.pageOfItems[index];
+    this.setState({ selectedBook: book, bookSelected: true });
   }
 
   render() {
     return (
-
+      <div>
+        {this.state.bookSelected ? (
+          <Book
+            item={this.state.selectedBook}
+            selectBookProp={this.onBookSelect}
+          />
+        ) : (
           <div>
             <ul className="book-list">
-              {this.props.books.map((book, index) => (
+              {this.state.pageOfItems.map((book, index) => (
                 <li key={index}>
                   <Book
                     item={book}
+                    selectBookProp={() => this.onBookSelect(index)}
                   />
                 </li>
               ))}
             </ul>
+            <JwPagination
+              items={this.props.books}
+              onChangePage={this.onChangePage}
+              labels={customLabels}
+              disableDefaultStyles={true}
+            />
+          </div>
+        )}
       </div>
-            
-        )
-      };
+    );
   }
-
+}
