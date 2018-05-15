7c46b93eed57dc8377c361f1175070d8214bb9f0
Laurent Milne <lmilne360@gmail.com>
2018-05-15 14:18:09 -0400

    Updated searchbar

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,30 +1,52 @@
 import React, { Component } from "react";
+import BookList from './BookList'
 
 const url = "http://localhost:4000/books";
 
 export default class Searchbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { books: [] };
+    this.state = { search: "", books: [], isloading: true };
     this.getBooks = this.getBooks.bind(this);
     this.getBooks();
   }
 
   getBooks() {
-
     fetch(url)
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        this.setState({ books: data});
+        this.setState({ books: data, isloading: false });
       });
   }
 
+  onChange = e => {
+    this.setState({ search: e.target.value });
+  };
+
   render() {
+    const { search, isloading } = this.state;
+    const filterBooks = this.state.books.filter(book => {
+      return book.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+
+    if (isloading) {
+      return <p>Loading...</p>;
+    }
 
     return (
       <div>
-          {this.state.books}
+        <div className="searchbar">
+          <form onSubmit={this.searchBooks}>
+            <input
+              onChange={this.onChange}
+              type="text"
+              id="searchbar-input"
+              placeholder="Search books..."
+            />
+          </form>
+        </div>
+        {<BookList books={filterBooks} />}
       </div>
     );
   }
