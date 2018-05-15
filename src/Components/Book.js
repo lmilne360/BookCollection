bc3f97790db50a5407b28feea2f6663fe17cb7b7
Laurent Milne <lmilne360@gmail.com>
2018-05-15 14:22:17 -0400

    Added book selection

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -6,7 +6,7 @@ export default class Book extends Component {
     const { title, author, description, image } = this.props.item;
 
     return (
-      <div >
+      <div onClick= {this.props.selectBookProp}>
         <h3> {title} </h3>
         <img src={image} alt="Book cover image" onError={ (e) => {e.target.src= `http://${image}`}}/>
       </div>
